a7de24d8d54c95eeb4dc62521b4015f4ce99e424
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 10:22:13 +0100

    Add IPerson interface

---
+++ Project (part A)/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project__part_A_
+{
+    internal interface IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+    }
+}
