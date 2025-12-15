057a6ad6fa68e256cdac5bfd659b95dd5b41f5c0
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:10:52 +0100

    Add TestClass for Artist class

---
+++ TestProject/TestArtist.cs
@@ -0,0 +1,29 @@
+﻿using Project__part_A_;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestArtist
+    {
+        private Artist artist;
+
+        //Сетап
+        [TestInitialize]
+        public void Setup()
+        {
+            artist = new Artist();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            artist = null;
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
