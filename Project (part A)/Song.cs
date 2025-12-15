5ec260b9a09f5d7ff5eb54757eefb02e1e520eb0
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 10:44:13 +0100

    Add Song class

---
+++ Project (part A)/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project__part_A_
+{
+    internal class Song
+    {
+        public string SongName { get; set; }
+        public int TotalPlays { get; set; }
+
+        public Band Band { get; set; }
+        public Genre Genre { get; set; }
+
+        public Song(string name, Band band, Genre genre)
+        {
+            SongName = name;
+            Band = band;
+            Genre = genre;
+        }
+    }
+}
