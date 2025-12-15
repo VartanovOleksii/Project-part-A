5f24cf9157553885a93cbbfc9662353cd111ff11
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:35:03 +0100

    Add Name, Age, Salary testcase

--- TestProject/TestArtist.cs
+++ TestProject/TestArtist.cs
@@ -22,8 +22,95 @@ namespace TestProject
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void Name_empty_input()
         {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => artist.Name = "");
+        }
+
+        [TestMethod]
+        [DataRow ("a")] //less than 3 symbols
+        [DataRow ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")] //more than 50 symbols
+        public void Name_incorrect_name(string s)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => artist.Name = s);
+        }
+
+        [TestMethod]
+        public void Name_correct_name()
+        {
+            //Arrange
+            string name = "Long Afternoon";
+            string expected = name;
+
+            //Act
+            artist.Name = name;
+
+            //Assert
+            Assert.AreEqual(expected, artist.Name);
+        }
+        
+        [TestMethod]
+        [DataRow (16)] //less than 18
+        [DataRow (100)] //more than 95
+        public void Age_incorrect_age(int age)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => artist.Age = age);
+        }
+
+        [TestMethod]
+        public void Age_correct_age()
+        {
+            //Arrange
+            int age = 21;
+            int expected = age;
+
+            //Act
+            artist.Age = age;
+
+            //Assert
+            Assert.AreEqual(expected, artist.Age);
+        }
+
+        [TestMethod]
+        [DataRow (-20)] //less or equal 0
+        [DataRow (0)] //less or equal 0
+        public void Salary_incorrect_salary(double salary)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => artist.Salary = salary);
+        }
+
+        [TestMethod]
+        public void Salary_correct_salary()
+        {
+            //Arrange
+            double salary = 150000;
+            double expected = salary;
+
+            //Act
+            artist.Salary = salary;
+
+            //Assert
+            Assert.AreEqual(expected, artist.Salary, 0.001);
         }
     }
 }
