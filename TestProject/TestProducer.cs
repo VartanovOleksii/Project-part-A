58d37c89707eafc316d25a2801c452f95c6077b8
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:13:40 +0100

    Add TetsClass for Producer class

---
+++ TestProject/TestProducer.cs
@@ -0,0 +1,29 @@
+﻿using Project__part_A_;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestProducer
+    {
+        private Producer producer;
+
+        //Сетап
+        [TestInitialize]
+        public void Setup()
+        {
+            producer = new Producer();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            producer = null;
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
