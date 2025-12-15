b2f9cfb09b80a3b8406e10da328c64a5ed21a934
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:16:54 +0100

    Add TestClass for Song class

---
+++ TestProject/TestSong.cs
@@ -0,0 +1,29 @@
+﻿using Project__part_A_;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestSong
+    {
+        private Song song;
+
+        //Сетап
+        [TestInitialize]
+        public void Setup()
+        {
+            song = new Song();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            song = null;
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
