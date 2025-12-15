3a7c40db252e0c84fecd6ee3d229e1e52e79ac4e
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:12:23 +0100

    Add TestClass for Band class

---
+++ TestProject/TestBand.cs
@@ -0,0 +1,29 @@
+﻿using Project__part_A_;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestBand
+    {
+        private Band band;
+
+        //Сетап
+        [TestInitialize]
+        public void Setup()
+        {
+            band = new Band();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            band = null;
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
