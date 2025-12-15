5e81620fb150316765d4e13f572e37638ab48f8e
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 10:36:29 +0100

    Add Band class

---
+++ Project (part A)/Band.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project__part_A_
+{
+    internal class Band
+    {
+        public string BandName { get; set; }
+        public int MonthlyListening { get; set; }
+        public List<Producer> Producers { get; set; }
+        public List<Artist> Artists { get; set; }
+
+        public Band(string name, List<Producer> producers)
+        {
+            BandName = name;
+            Producers = producers;
+
+            Artists = new List<Artist>();
+        }
+    }
+}
