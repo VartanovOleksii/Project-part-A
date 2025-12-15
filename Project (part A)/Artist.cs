c5ea75757bd51768ea66d0bc255449ae7696d80c
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 10:22:21 +0100

    Add Artist class

---
+++ Project (part A)/Artist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project__part_A_
+{
+    internal class Artist: IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public string Instrument { get; set; }
+        public bool IsActive { get; set; }
+        public int FanCount { get; set; }
+
+        public Artist(string name, int age, double salary, string instrument)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+            Instrument = instrument;
+        }
+    }
+}
