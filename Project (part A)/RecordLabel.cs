085d61a894edbbed6af5f75890246d7b02e704f0
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 10:52:07 +0100

    Add RecordLabel class

---
+++ Project (part A)/RecordLabel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project__part_A_
+{
+    internal class RecordLabel
+    {
+        public string RecordLabelName { get; set; }
+        public List<Band> Bands { get; set; }
+
+        public bool SignBand(Band band)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Revenue(Band band, double rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecordLabel(string name, List<Band> bands)
+        {
+            RecordLabelName = name;
+            Bands = bands;
+        }
+    }
+}
