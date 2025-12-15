57df0c4858073bb4270e0591500e762348856065
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-12-15 11:15:45 +0100

    Add TestClass for RecordLabel class

---
+++ TestProject/TestRecordLabel.cs
@@ -0,0 +1,29 @@
+﻿using Project__part_A_;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestRecordLabel
+    {
+        private RecordLabel recordLabel;
+
+        //Сетап
+        [TestInitialize]
+        public void Setup()
+        {
+            recordLabel = new RecordLabel();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            recordLabel = null;
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
